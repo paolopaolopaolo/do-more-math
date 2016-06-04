b6ac44c31e8f512a6bd8f7685b186e13ecaa0dc3
Dean Mercado <dean@yeti.co>
2016-06-04 12:26:08 -0700

    Initial Commit

---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "_P:zq<XnJzuF?zDUBY!XuEpyB=|A@8E1SRcd%#WT"
+
+ALLOWED_HOSTS = ['*']
